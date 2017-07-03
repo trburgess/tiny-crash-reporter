7b59bf23315ad0351c76e136cd36e3fabeda9744
Tom Burgess <tom.burgess@adminisatorsmbp.home>
2017-07-03 21:35:41 +0100

    Complete functionality of Handler
    Update Tests to test output of methods

---
+++ src/Handler.php
@@ -0,0 +1,59 @@
+<?php
+namespace TinyCrashReporter;
+
+class Handler
+{
+    /**
+     * @return Handler
+     */
+    public static function register()
+    {
+        $handler = new static;
+        set_error_handler([$handler, 'errorHandler']);
+        set_exception_handler([$handler, 'exceptionHandler']);
+
+        return $handler;
+    }
+
+    /**
+     * @param $errorNumber
+     * @param $errorString
+     * @param string $errorFile
+     * @param int $errorLine
+     * @return bool
+     */
+    public function errorHandler(
+        $errorNumber,
+        $errorString,
+        $errorFile = '',
+        $errorLine = 0
+    ) {
+        print $this->formatMessage($errorNumber, $errorString);
+        // continue to be process by the default PHP error handler
+        return false;
+    }
+
+    /**
+     * Exception handler callback.
+     *
+     * @param \Throwable|\Exception $exception the exception was was thrown
+     * \Throwable PHP Version >= 7
+     * \Exception PHP Version <= 5.6
+     *
+     * @return void
+     */
+    public function exceptionHandler($exception)
+    {
+        print $this->formatMessage(get_class($exception), $exception->getMessage());
+    }
+
+    /**
+     * @param $what
+     * @param $why
+     * @return string
+     */
+    protected function formatMessage($what, $why)
+    {
+        return $what . ': ' . $why;
+    }
+}
--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -2,10 +2,15 @@
 namespace TintCrashReporter\Tests;
 
 use Exception;
+use TinyCrashReporter\Handler;
 
 class HandlerTest extends \PHPUnit_Framework_TestCase
 {
     const ERROR_MESSAGE = 'Error Message';
+
+    /**
+     * @var Handler
+     */
     protected $handler;
 
     protected function setUp()
@@ -15,11 +20,28 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
 
     public function testErrorHandler()
     {
-        $this->handler->errorHandler(E_WARNING, self::ERROR_MESSAGE, 'errorfile.php', 1);
+        $this->expectOutputString($this->formatMessage(E_WARNING, self::ERROR_MESSAGE));
+        $this->handler->errorHandler(
+            E_WARNING,
+            self::ERROR_MESSAGE,
+            'errorfile.php',
+            1
+        );
     }
 
     public function testExceptionHandler()
     {
+        $this->expectOutputString($this->formatMessage(Exception::class, self::ERROR_MESSAGE));
         $this->handler->exceptionHandler(new Exception(self::ERROR_MESSAGE));
+    }
+
+    /**
+     * @param $what
+     * @param $why
+     * @return string
+     */
+    protected function formatMessage($what, $why)
+    {
+        return $what . ': ' . $why;
     }
 }
